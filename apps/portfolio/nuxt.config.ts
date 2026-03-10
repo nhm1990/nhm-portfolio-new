817acea64c37ac818f8fee534fa1be3de0b754e7
nhm1990 <nhm1990@users.noreply.github.com>
2026-03-10 19:56:48 +0100

    fix: test

--- apps/portfolio/nuxt.config.ts
+++ apps/portfolio/nuxt.config.ts
@@ -30,7 +30,7 @@ export default defineNuxtConfig({
       { code: 'de', name: 'Deutsch', file: 'de.json' },
     ],
     vueI18n: './i18n.config.ts',
-    detectBrowserLanguage: false,
+    detectBrowserLanguage: false, //test
   },
 
   fonts: {
